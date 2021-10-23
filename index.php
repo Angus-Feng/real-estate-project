245655a85cb6c83449a6b5b3e9799d84eaadb937
Feng <85354711+Angus-Feng@users.noreply.github.com>
2021-10-23 17:44:19 -0400

    Reclone the repo to fix a branch issue
    update scrum 2021/10/22 for all team members
    draft code for user.php done (not tested)

--- index.php
+++ index.php
@@ -6,12 +6,15 @@ require_once 'utils.php';
 
 // Define app routes below
 require_once 'admin.php';
+require_once 'user.php';
 
 
-// Testing
+// Define app routes
 $app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello " . $args['name'] . " from phprealestateproject.");
+    return $response->write("Hello " . $args['name']);
 });
+
+
 
 // Run app
 $app->run();
