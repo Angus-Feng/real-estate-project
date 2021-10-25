ce5db2f12914a9dd3a005c6150d6e7ae1c530ef8
latchmanc6 <latchmanc6@gmail.com>
2021-10-24 21:05:33 -0400

    Working on admin interface
    - User (buyer/broker) CRUD 90% finished (working on special cases)
    - Property CRUD in progress
    - TODO: Finish property CRUD
    - TODO: Broker approval

--- admin.php
+++ admin.php
@@ -4,6 +4,315 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
-// $app->get('/admin/user/list', function .....);
+// Default admin page
+$app->get('/admin', function ($request, $response, $args) {
+    return $this->view->render($response, 'admin/index_admin.html.twig');
+});
 
-// $app->get('/admin/user/list', function .....);
+
+// Users (Buyers/Brokers)
+
+// View a list of all users and their information
+$app->get('/admin/users/list', function ($request, $response, $args) {
+    $userList = DB::query("SELECT * FROM users");
+    return $this->view->render($response, 'admin/users_list.html.twig', ['usersList' => $userList]);
+});
+
+// Add user: GET
+$app->get('/admin/users/add/{userType:buyer|broker}', function ($request, $response, $args) {
+    $userType = $args['userType'];
+    return $this->view->render($response, 'admin/users_add.html.twig', ['userType' => $userType]);
+});
+
+// Add user: POST
+$app->post('/admin/users/add/{userType:buyer|broker}', function ($request, $response, $args) {
+    $userType = $args['userType'];
+
+    $email = $request->getParam('email');
+    $password1 = $request->getParam('password1');
+    $password2 = $request->getParam('password2');
+    // Check if user is a broker 
+    if ($userType === 'broker') {
+        $licenseNo = $request->getParam('licenseNo');
+        $firstName = $request->getParam('firstName');
+        $lastName = $request->getParam('lastName');
+        $phone = $request->getParam('phone');
+        $company = $request->getParam('company');
+        $jobTitle = $request->getParam('jobTitle');
+        $appartmentNo = $request->getParam('appartmentNo');
+        $streetNo = $request->getParam('streetNo');
+        $streetName = $request->getParam('streetName');
+        $city = $request->getParam('city');
+        $province = $request->getParam('province');
+        $postalCode = $request->getParam('postalCode');
+    }
+
+    $errorList = [];
+
+    $emailVerification = verfiyEmail($email);
+    if ($emailVerification !== TRUE) {
+        $errorList[] = $emailVerification;
+    }
+    $verifyPasswords = verifyPasswords($password1, $password2);
+    if ($verifyPasswords !== TRUE) {
+        $errorList[] = $verifyPasswords;
+    }
+    // Check if user is a broker 
+    if ($userType === 'broker') {
+        $verifyLicenseNo = verifyLicenseNo($licenseNo);
+        if ($verifyLicenseNo !== TRUE) {
+            $errorList[] = $verifyLicenseNo;
+        }
+        $verifyFirstName = verifyFirstName($firstName);
+        if ($verifyFirstName !== TRUE) {
+            $errorList[] = $verifyFirstName;
+        }
+        $verifyLastName = verifyLastName($lastName);
+        if ($verifyLastName !== TRUE) {
+            $errorList[] = $verifyLastName;
+        }
+        $verifyPhone = verifyPhone($phone);
+        if ($verifyLicenseNo !== TRUE) {
+            $errorList[] = $verifyPhone;
+        }
+        $verifyCompany = verifyCompany($company);
+        if ($verifyCompany !== TRUE) {
+            $errorList[] = $verifyCompany;
+        }
+        $verifyJobTitle = verifyJobTitle($jobTitle);
+        if ($verifyJobTitle !== TRUE) {
+            $errorList[] = $verifyJobTitle;
+        }
+        $verifyAppartmentNo = verifyAppartmentNo($appartmentNo);
+        if ($verifyAppartmentNo !== TRUE) {
+            $errorList[] = $verifyAppartmentNo;
+        }
+        $verifyStreetNo = verifyStreetNo($streetNo);
+        if ($verifyStreetNo !== TRUE) {
+            $errorList[] = $verifyStreetNo;
+        }
+        $verifyStreetName = verifyStreetName($streetName);
+        if ($verifyStreetName !== TRUE) {
+            $errorList[] = $verifyStreetName;
+        }
+        $verifyCityName = verifyCityName($city);
+        if ($verifyCityName !== TRUE) {
+            $errorList[] = $verifyCityName;
+        }
+        $verifyProvince = verifyProvince($province);
+        if ($verifyProvince !== TRUE) {
+            $errorList[] = $verifyProvince;
+        }
+        $verifyPostalCode = verifyPostalCode($postalCode);
+        if ($verifyPostalCode !== TRUE) {
+            $errorList[] = $verifyPostalCode;
+        }
+    }
+
+    if ($errorList) {
+        return $this->view->render($response, 'admin/users_add.html.twig', ['errorList' => $errorList, 'userType' => $userType]);
+    }
+
+    if ($userType === 'buyer') {
+        DB::insert('users', ['email' => $email, 'password' => $password1]);
+    } else {
+        DB::insert('users', [
+            'email' => $email, 
+            'password' => $password1, 
+            'role' => 'broker',
+            'licenseNo' => $licenseNo, 
+            'firstName' => $firstName, 
+            'lastName' => $lastName, 
+            'phone' => $phone,
+            'company' => $company,
+            'jobTitle' => $jobTitle,
+            'appartmentNo' => $appartmentNo,
+            'streetNo' => $streetNo,
+            'city' => $city,
+            'province' => $province,
+            'postalCode' => $postalCode
+        ]);
+    }
+
+    return $this->view->render($response, 'admin/modification_success.html.twig');
+});
+
+// Edit user: GET
+$app->get('/admin/users/edit/{id:[0-9]+}', function ($request, $response, $args) {
+    $user = DB::queryFirstRow("SELECT * FROM users WHERE id=%d", $args['id']);
+    if (!$user) {
+        // $response = $response->withStatus(404);
+        return $this->view->render($response, '404_error.html.twig');
+    }
+    return $this->view->render($response, 'admin/users_edit.html.twig', ['user' => $user]);
+});
+
+// Edit user: POST
+$app->post('/admin/users/edit/{id:[0-9]+}', function ($request, $response, $args) {
+    $user = DB::queryFirstRow("SELECT * FROM users WHERE id=%d", $args['id']);
+
+    // Check if id is an admin and stop it from being editable 
+    if ($user['role'] === 'admin') { // TODO
+        return $this->view->render($response, 'admin/users_edit.html.twig', ['user' => $user]);
+    }
+
+    $id = $args['id'];
+    $email = $request->getParam('email');
+    $password1 = $request->getParam('password1');
+    $password2 = $request->getParam('password2');
+    // Check if user is a broker 
+    if ($user['role'] === 'broker') {
+        $licenseNo = $request->getParam('licenseNo');
+        $firstName = $request->getParam('firstName');
+        $lastName = $request->getParam('lastName');
+        $phone = $request->getParam('phone');
+        $company = $request->getParam('company');
+        $jobTitle = $request->getParam('jobTitle');
+        $appartmentNo = $request->getParam('appartmentNo');
+        $streetNo = $request->getParam('streetNo');
+        $streetName = $request->getParam('streetName');
+        $city = $request->getParam('city');
+        $province = $request->getParam('province');
+        $postalCode = $request->getParam('postalCode');
+    }
+
+    $errorList = [];
+
+    $emailVerification = verfiyEmailUpdate($email, $id);
+    if ($emailVerification !== TRUE) {
+        $errorList[] = $emailVerification;
+    }
+    $verifyPasswords = verifyPasswords($password1, $password2);
+    if ($verifyPasswords !== TRUE) {
+        $errorList[] = $verifyPasswords;
+    }
+    // Check if user is a broker 
+    if ($user['role'] === 'broker') {
+        $verifyLicenseNo = verifyLicenseNo($licenseNo, $id);
+        if ($verifyLicenseNo !== TRUE) {
+            $errorList[] = $verifyLicenseNo;
+        }
+        $verifyFirstName = verifyFirstName($firstName);
+        if ($verifyFirstName !== TRUE) {
+            $errorList[] = $verifyFirstName;
+        }
+        $verifyLastName = verifyLastName($lastName);
+        if ($verifyLastName !== TRUE) {
+            $errorList[] = $verifyLastName;
+        }
+        $verifyPhone = verifyPhone($phone);
+        if ($verifyLicenseNo !== TRUE) {
+            $errorList[] = $verifyPhone;
+        }
+        $verifyCompany = verifyCompany($company);
+        if ($verifyCompany !== TRUE) {
+            $errorList[] = $verifyCompany;
+        }
+        $verifyJobTitle = verifyJobTitle($jobTitle);
+        if ($verifyJobTitle !== TRUE) {
+            $errorList[] = $verifyJobTitle;
+        }
+        $verifyAppartmentNo = verifyAppartmentNo($appartmentNo);
+        if ($verifyAppartmentNo !== TRUE) {
+            $errorList[] = $verifyAppartmentNo;
+        }
+        $verifyStreetNo = verifyStreetNo($streetNo);
+        if ($verifyStreetNo !== TRUE) {
+            $errorList[] = $verifyStreetNo;
+        }
+        $verifyStreetName = verifyStreetName($streetName);
+        if ($verifyStreetName !== TRUE) {
+            $errorList[] = $verifyStreetName;
+        }
+        $verifyCityName = verifyCityName($city);
+        if ($verifyCityName !== TRUE) {
+            $errorList[] = $verifyCityName;
+        }
+        $verifyProvince = verifyProvince($province);
+        if ($verifyProvince !== TRUE) {
+            $errorList[] = $verifyProvince;
+        }
+        $verifyPostalCode = verifyPostalCode($postalCode);
+        if ($verifyPostalCode !== TRUE) {
+            $errorList[] = $verifyPostalCode;
+        }
+    }
+
+    if ($errorList) {
+        if (!$user) {
+            $response = $response->withStatus(404);
+            return $this->view->render($response, '404_error.html.twig');
+        }
+        return $this->view->render($response, 'admin/users_edit.html.twig', ['errorList' => $errorList, 'user' => $user]);
+    }
+
+    if ($user['role'] === 'buyer') {
+        DB::update('users', ['email' => $email, 'password' => $password1], "id=%d", $id);
+    } else {
+        DB::update('users', [
+            'email' => $email, 
+            'password' => $password1, 
+            'licenseNo' => $licenseNo, 
+            'firstName' => $firstName, 
+            'lastName' => $lastName, 
+            'phone' => $phone,
+            'company' => $company,
+            'jobTitle' => $jobTitle,
+            'appartmentNo' => $appartmentNo,
+            'streetNo' => $streetNo,
+            'city' => $city,
+            'province' => $province,
+            'postalCode' => $postalCode
+        ], "id=%d", $id);
+    }
+
+    return $this->view->render($response, 'admin/modification_success.html.twig');
+});
+
+// Delete user: GET
+$app->get('/admin/users/delete/{id:[0-9]+}', function ($request, $response, $args) {
+    $id = $args['id'];
+    // if (!isset($_SESSION['user']) || $_SESSION['user']['role'] != 'admin') {
+    //     $app->redirect('/forbidden');
+    //     return;
+    // }
+
+    $user = DB::queryFirstRow("SELECT * FROM users WHERE id=%i", $id);
+    if (!$user) {
+        // $response = $response->withStatus(404);
+        return $this->view->render($response, '404_error.html.twig');
+    }
+    return $this->view->render($response, 'admin/users_delete.html.twig', ['user' => $user]);
+});
+
+// Delete user: POST
+$app->post('/admin/users/delete/{id:[0-9]+}', function ($request, $response, $args) {
+    $user = DB::queryFirstRow("SELECT * FROM users WHERE id=%d", $args['id']);
+
+    // Check if id is an admin and stop it from being editable 
+    if ($user['role'] === 'admin') { // TODO
+        return $this->view->render($response, '404_error.html.twig');
+    }
+
+    DB::delete('users', 'id=%d', $args['id']);
+
+    return $this->view->render($response, 'admin/modification_success.html.twig');
+});
+
+//-----------------------------------------------------------------------------------------------------------------
+
+
+// Properties
+
+// View a list of all properties and their information
+$app->get('/admin/property/list', function ($request, $response, $args) {
+    $propertyList = DB::query("SELECT * FROM properties");
+    return $this->view->render($response, 'admin/property_list.html.twig', ['propertyList' => $propertyList]);
+});
+
+// Add property: GET
+$app->get('/admin/property/add', function ($request, $response, $args) {
+    return $this->view->render($response, 'admin/property_add.html.twig'); // TODO: Add multiple image upload
+});
+
+//-----------------------------------------------------------------------------------------------------------------
--- utils.php
+++ utils.php
@@ -4,8 +4,7 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
-function verifyUploadedPhoto($photo, &$filePath)
-{
+function verifyUploadedPhoto($photo, &$filePath) {
 
     if ($photo->getError() !== UPLOAD_ERR_OK) {
         return "Upload failed." . $photo->getError();
@@ -38,5 +37,146 @@ function verifyUploadedPhoto($photo, &$filePath)
     }
 
     $filePath = "uploads/" . mb_ereg_replace("[^\w\s\d\)]", '_', pathinfo($photo->getClientFilename(), PATHINFO_FILENAME)) . "." . $ext;
+    return TRUE;
+}
+
+function verfiyEmail($email) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return "Please enter a valid email";
+    } else {
+        $checkEmail = DB::query("SELECT email FROM users WHERE email = '$email'");
+        if ($checkEmail) {
+            return "This email is already registered.";
+        }
+    }
+    return TRUE;
+}
+
+function verfiyEmailUpdate($email, $id) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return "Please enter a valid email";
+    } else {
+        $checkEmail = DB::query("SELECT email FROM users WHERE email = '$email' AND id != $id");
+        if ($checkEmail) {
+            return "This email is already registered.";
+        }
+    }
+    return TRUE;
+}
+
+function verifyPasswords($password1, $password2) {
+    if (strlen($password1) < 8 || strlen($password1) > 25
+        || (preg_match("/[A-Z]/", $password1) !== 1)
+        || (preg_match("/[a-z]/", $password1) !== 1)
+        || (preg_match("/[0-9]/", $password1) !== 1)
+        || (preg_match("/[#?!@$%^&*-]/", $password1) !== 1)
+    ) {
+        return "Minimum 8 characters with 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character.";
+    }
+
+    if ($password1 !== $password2) {
+        return 'The passwords you have entered do not match.';
+    }
+    return TRUE;
+}
+
+function verifyLicenseNo($licenseNo) {
+    if (!preg_match('/^[A-Z]{3}[0-9]{6}[A-Z]{2}/', $licenseNo)) {
+        return "Please enter a valid license number";
+    } else {
+        $checkLicenseNo = DB::query("SELECT licenseNo FROM users WHERE licenseNo = '$licenseNo'");
+        if ($checkLicenseNo) {
+            return "This license number is already registered.";
+        }
+    }
+    return TRUE;
+}
+
+function verifyLicenseNoUpdate($licenseNo, $id) {
+    if (!preg_match('/^[A-Z]{3}[0-9]{6}[A-Z]{2}/', $licenseNo)) {
+        return "Please enter a valid license number";
+    } else {
+        $checkLicenseNo = DB::query("SELECT licenseNo FROM users WHERE licenseNo = '$licenseNo' AND id != $id");
+        if ($checkLicenseNo) {
+            return "This license number is already registered.";
+        }
+    }
+    return TRUE;
+}
+
+function verifyFirstName($firstName) { //TEST REGEX
+    if (!preg_match('/^[a-zA-Z\.\'\-]+$/', $firstName) || strlen($firstName) < 1 || strlen($firstName) > 100) {
+        return "First name must be between 1 - 100 characters long and can only contain letters, periods, apostrophes and hyphens.";
+    }
+    return TRUE;
+}
+
+function verifyLastName($lastName) { //TEST REGEX
+    if (!preg_match('/^[a-zA-Z\.\'\-]+$/', $lastName) || strlen($lastName) < 1 || strlen($lastName) > 100) {
+        return "Last name must be between 1 - 100 characters long and can only contain letters, periods, apostrophes and hyphens.";
+    }
+    return TRUE;
+}
+
+function verifyPhone($phone) { //TEST REGEX
+    if (!preg_match('/^[0-9]{10}$/', $phone)) {
+        return "Phone number must be 10 digits.";
+    }
+    return TRUE;
+}
+
+function verifyCompany($company) { //TEST REGEX
+    if (!preg_match('/^[a-zA-Z\.\'\-\s]+$/', $company) || strlen($company) < 1 || strlen($company) > 100) {
+        return "Company name must be between 1 - 100 characters long and can only contain letters, periods, apostrophes and hyphens.";
+    }
+    return TRUE;
+}
+
+function verifyJobTitle($jobTitle) { //TEST REGEX
+    if (!preg_match('/^[a-zA-Z\.\'\-\s]+$/', $jobTitle) || strlen($jobTitle) < 1 || strlen($jobTitle) > 100) {
+        return "Job title must be between 1 - 100 characters long and can only contain letters, periods, apostrophes and hyphens.";
+    }
+    return TRUE;
+}
+
+function verifyAppartmentNo($appartmentNo) { //TEST REGEX
+    if (!preg_match('/^[0-9]+$/', $appartmentNo)) {
+        return "Appartment number can only be made up of numbers.";
+    }
+    return TRUE;
+}
+
+function verifyStreetNo($streetNo) { //TEST REGEX
+    if (!preg_match('/^[0-9]+$/', $streetNo)) {
+        return "Street number can only be made up of numbers.";
+    }
+    return TRUE;
+}
+
+function verifyStreetName($streetName) { //TEST REGEX
+    if (!preg_match('/^[a-zA-Z\.\'\-\s]+$/', $streetName) || strlen($streetName) < 1 || strlen($streetName) > 100) {
+        return "Street name must be between 1 - 100 characters long and can only contain letters, periods, apostrophes and hyphens.";
+    }
+    return TRUE;
+}
+
+function verifyCityName($city) { //TEST REGEX
+    if (!preg_match('/^[a-zA-Z\.\'\-\s]+$/', $city) || strlen($city) < 1 || strlen($city) > 100) {
+        return "City name must be between 1 - 100 characters long and can only contain letters, periods, apostrophes and hyphens.";
+    }
+    return TRUE;
+}
+
+function verifyProvince($province) { //TEST REGEX
+    if (!preg_match('/^[A-Z]{2}$/', $province)) {
+        return "Province must be in the following format: QC.";
+    }
+    return TRUE;
+}
+
+function verifyPostalCode($postalCode) { //TEST REGEX
+    if (!preg_match('/^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/', $postalCode)) {
+        return "Postal code must be in the following format: H9X3L9.";
+    }
     return TRUE;
 }
